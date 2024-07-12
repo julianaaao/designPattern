5181ad792ea88b70e4689d893868943d63e7d909
juahoh <ohjuah0119.hanpass@gmail.com>
2024-07-12 18:02:26 +0900

    chapter 5 - classic singleton

---
+++ src/main/java/com/headfirst/api/chapter5/classic/Singleton.java
@@ -0,0 +1,18 @@
+package com.headfirst.api.chapter5.classic;
+
+public class Singleton {
+    private static Singleton uniqueInstance;
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        if(uniqueInstance == null){
+            uniqueInstance = new Singleton();
+        }
+        return uniqueInstance;
+    }
+
+    public String getDescription(){
+        return "I'm a classic Singleton";
+    }
+}
